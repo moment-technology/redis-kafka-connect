6d50336b499f9c7bdae2ad06b30b1615c18596f7
nanasingh <nana@withmoment.com>
2024-09-05 11:56:07 -0400

    remove changes to publish

--- core/redis-kafka-connect/src/main/java/com/redis/kafka/connect/sink/Publish.java
+++ core/redis-kafka-connect/src/main/java/com/redis/kafka/connect/sink/Publish.java
@@ -1,11 +1,9 @@
 package com.redis.kafka.connect.sink;
 
-import java.util.Objects;
 import java.util.function.Function;
 
 import io.lettuce.core.RedisFuture;
 import io.lettuce.core.api.async.BaseRedisAsyncCommands;
-import io.lettuce.core.api.async.RedisKeyAsyncCommands;
 
 import com.redis.spring.batch.writer.operation.AbstractKeyWriteOperation;
 
@@ -17,13 +15,9 @@ public class Publish<K, V, T> extends AbstractKeyWriteOperation<K, V, T> {
         this.valueFunction = function;
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     protected RedisFuture<Long> execute(BaseRedisAsyncCommands<K, V> commands, T item, K key) {
         V value = valueFunction.apply(item);
-        if (Objects.isNull(value)) {
-            return ((RedisKeyAsyncCommands<K, V>) commands).del(key);
-        }
         return ((BaseRedisAsyncCommands<K, V>) commands).publish(key, value);
     }
 
